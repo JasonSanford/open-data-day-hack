e3613fdb3dce0e93be45c8d2c62bf02051974c8a
Jason Sanford <jasonsanford@gmail.com>
2010-12-01 22:55:44 -0500

    Creating a map, centering on Trade & Tryon

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-/* Author: 
+/* Author: Jason Sanford
 
 */
-
-
+var odd = {
+	
+};
+$(function(){
+	
+	odd.map = new google.maps.Map(document.getElementById("map-canvas"), {
+		zoom: 9,
+		center: new google.maps.LatLng(35.22720562368099, -80.84311660003662),
+		streetViewControl: false,
+		mapTypeId: "terrain"
+	});
+	
+});
 
 
 
