cfbc03410c1b8cb2be91df2f629dc96b42f6c2eb
Jason Sanford <jasonsanford@gmail.com>
2010-12-01 23:10:14 -0500

    Just a few style and map changes. No fun stuff yet.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@
 */
 var odd = {
 	
+	apiBase: "http://maps.co.mecklenburg.nc.us/rest/v1/"
+	
 };
 $(function(){
 	
 	odd.map = new google.maps.Map(document.getElementById("map-canvas"), {
-		zoom: 9,
+		zoom: 10,
 		center: new google.maps.LatLng(35.22720562368099, -80.84311660003662),
 		streetViewControl: false,
 		mapTypeId: "terrain"
