04a548e9ab0f4bdd95e2e208c1f5bbff062e0df8
Jason Sanford <jasonsanford@gmail.com>
2010-12-09 00:22:31 -0500

    Now properly filtering spatial and non-spatial attributes.

--- js/script.js
+++ js/script.js
@@ -27,23 +27,6 @@ var odd = {
 	}
 };
 
-/*
-This Works
-We'll need to reproject our point from 4326 to 2264
-http://maps.co.mecklenburg.nc.us/rest/v2/ws_geo_bufferpoint.php?x=1457321.92512878&y=525329.668774016&srid=2264&geotable=building_permits&parameters=date_issued%3E%272010-01-01%27&order=&limit=1000&format=json&fields=project_name,project_address,square_footage,construction_cost,type_of_building,job_status,date_issued,mat_parcel_id,occupancy,st_asgeojson%28transform%28the_geom,4326%29,6%29+as+geojson&distance=1000
-
-http://maps.co.mecklenburg.nc.us/rest/v2/ws_geo_bufferpoint.php
-?x=1457321.92512878
-&y=525329.668774016
-&srid=2264
-&geotable=building_permits
-&parameters=date_issued%3E%272010-01-01%27&order=
-&limit=1000
-&format=json
-&fields=project_name,project_address,square_footage,construction_cost,type_of_building,job_status,date_issued,mat_parcel_id,occupancy,st_asgeojson%28transform%28the_geom,4326%29,6%29+as+geojson
-&distance=1000
-*/
-
 /* document ready */
 
 $(function(){
@@ -80,7 +63,7 @@ $(function(){
 			$("#footage").html(ui.values[0] + " - " + ui.values[1] + " ft.<sup>2</sup>");
 		},
 		stop: function(event, ui){
-			updateResults();
+			updateResults(true);
 		}
 	}).slider("disable");
 	
@@ -93,7 +76,7 @@ $(function(){
 			$("#cost").html("$" + ui.values[0] + " - $" + ui.values[1]);
 		},
 		stop: function(event, ui){
-			updateResults();
+			updateResults(true);
 		}
 	});
 	
@@ -102,7 +85,7 @@ $(function(){
 	$(".param").click(function(){
 		$(this).find("div.slider").slider($(this).hasClass("active") ? "disable" : "enable");
 		$(this).toggleClass("active");
-		updateResults();
+		updateResults(true);
 	});
 	
 });
@@ -113,40 +96,7 @@ $(window).resize(function(){
 
 /* lives */
 
-/*$(".layer-check").live("click", function(){
-	var layerName = $(this).attr("id").split("-")[1];
-	if ($(this).attr("checked")){
-		updateLayer(layerName);
-	}else{
-		hideLayer(layerName);
-	}
-});*/
 
-/*$("#ul-layers .layer").live("click", function(){
-	if (!$(this).hasClass("expanded")){
-		// We haven't shown any fields for this layer yet
-		$(this).addClass("expanded");
-		var layerName = $(this).attr("id").split("-")[1];
-		var fields = '<ul id="fields-' + layerName + '">';
-		var layerLI$ = $(this).parent("li");
-		if (!odd.layers[layerName].fields.length){
-			$.getJSON(odd.apiBase + "ws_geo_listfields.php?geotable=" + layerName + "&format=json&callback=?", function(data){
-				$.each(data.rows, function(i,o){
-					odd.layers[layerName].fields.push(o.row.field_name);
-					fields += '<li><input type="checkbox" class="field-check" id="check-' + layerName + '-' + o.row.field_name + '" />&nbsp;' + o.row.field_name + ' (' + o.row.field_type + ')</li>';
-				});
-				fields += '</ul>';
-				layerLI$.append(fields);
-			});
-		}else{
-			$(this).parent("li").children("ul").show();
-		}
-	}else{
-		// We've already shown the fields for this layer, assume the user wants to hide fields
-		$(this).removeClass("expanded");
-		$(this).parent("li").children("ul").hide();
-	}
-});*/
 
 /* functions */
 
@@ -184,12 +134,12 @@ function updateSearchArea(){
 	}, 200);
 }
 
-function updateResults(){
+function updateResults(rerunSpatial){
 	if (!odd.distanceWidget)
 		return;
 	var removeThese = [];
 	$.each(odd.results, function(result_index, result){
-		if (distanceBetweenPoints(result.gVector.getPosition(), odd.distanceWidget.get("position")) > odd.distanceWidget.get("distance")){
+		if (rerunSpatial || distanceBetweenPoints(result.gVector.getPosition(), odd.distanceWidget.get("position")) > odd.distanceWidget.get("distance")){
 			removeThese.push(result.row.gid);
 		}
 	});
@@ -253,7 +203,6 @@ function addThese(these){
 			o.gVector.setIcon(odd.styles.results.normal.icon);
 		});
 		odd.results.push(o);
-		//html += '<div id="result-' + o.row.gid + '" class="result">' + o.row.project_name + '</div>';
 		$("#results").append('<div class="result-container"><div id="result-' + o.row.gid + '" class="result"><div class="field project_name">' + o.row.project_name + '</div><div class="field project_address">' + o.row.project_address + '</div><div class="field date_issued">' + o.row.date_issued + '</div><div class="field square_footage">' + o.row.square_footage + '</div><div class="field construction_cost">' + o.row.construction_cost + '</div><div class="field type_of_building">' + o.row.type_of_building + '</div><div class="field job_status">' + o.row.job_status + '</div><div class="clearit"></div></div></div>');
 	});
 }
@@ -275,105 +224,6 @@ function buildParams(){
 	});
 	return params;
 }
-
-/*function getLayers(){
-	
-	google.maps.event.removeListener(odd.tilesloaded);
-	
-	$.getJSON(odd.apiBase + "ws_geo_listlayers.php?format=json&callback=?", function(data){
-		$.each(data.rows, function(i,o){
-			odd.layers[o.row.layer_name] = {
-				features: [],
-				style: {
-					normal: {
-						strokeColor: "#000000",
-						strokeOpacity: 0.85,
-						strokeWeight: 4
-					},
-					highlight: {
-						strokeColor: "#ffff00",
-						strokeOpacity: 0.85,
-						strokeWeight: 6
-					}
-				},
-				fields: []
-			};
-			$("#ul-layers").append('<li><input type="checkbox" class="layer-check" id="check-' + o.row.layer_name + '" />&nbsp;<a class="layer" href="javascript:void(0);" id="a-' + o.row.layer_name + '">' + o.row.layer_name + '</a>&nbsp;<span style="display: none;" id="count-' + o.row.layer_name + '">0</span></li>');
-		});
-	});
-	
-}*/
-
-/*function createEnvelopeString(){
-	
-	var bounds = odd.map.getBounds();
-	var sw = bounds.getSouthWest();
-	var ne = bounds.getNorthEast();
-	return "ST_GeomFromText('POLYGON ((" + sw.lng() + " " + sw.lat() + ", " + sw.lng() + " " + ne.lat() + ", " + ne.lng() + " " + ne.lat() + ", " + ne.lng() + " " + sw.lat() + ", " + sw.lng() + " " + sw.lat() + "))', 4326)";
-	
-}*/
-
-/*function updateLayer(layerName){
-	
-	var otherFields = "";
-	$("#fields-" + layerName + " input").each(function(i, o){
-		if ($(this).attr("checked"))
-			otherFields += $(this).attr("id").split("-")[2] + ",";
-	});
-	
-	var otherParams = "";
-	
-	$.getJSON(odd.apiBase + "ws_geo_attributequery.php?geotable=" + layerName + "&fields=gid," + otherFields + "st_asgeojson(transform(simplify(the_geom,5),4326),6)+as+geojson&parameters=" + createEnvelopeString() + "+%26%26transform(the_geom,4326)" + otherParams + "+limit+1000&format=json&callback=?", function(data){
-		if (!parseInt(data.total_rows))
-			return;
-		$.each(data.rows, function(i, o){
-			var onMap = false;
-			$.each(odd.layers[layerName].features, function(i2, o2){
-				if (o.row.gid == o2.row.gid){
-					onMap = true;
-					return false;
-				}
-			});
-			if (onMap)
-				return;
-			o.gVector = new GeoJSON(o.row.geojson);
-			o.gVector.setOptions(odd.layers[layerName].style.normal);
-			o.gVector.setMap(odd.map);
-			google.maps.event.addListener(o.gVector, "mouseover", function(){
-				o.gVector.setOptions(odd.layers[layerName].style.highlight);
-			});
-			google.maps.event.addListener(o.gVector, "mouseout", function(){
-				o.gVector.setOptions(odd.layers[layerName].style.normal);
-			});
-			google.maps.event.addListener(o.gVector, "click", function(evt){
-				var content = '<table><tbody>';
-				for (prop in o.row){
-					if (prop != "geojson")
-						content += '<tr><td>' + prop + '</td><td>' + o.row[prop] + '</td></tr>';
-				}
-				content += '</tbody></table>';
-				odd.iw.setContent(content);
-				odd.iw.setPosition(evt.latLng || o.gVector.getPosition());
-				odd.iw.open(odd.map);
-			});
-			odd.layers[layerName].features.push(o);
-			$("#count-" + layerName).html(parseInt($("#count-" + layerName).html()) + 1).show();
-		});
-	});
-	
-}*/
-
-/*function hideLayer(layerName){
-	
-	$.each(odd.layers[layerName].features, function(i, o){
-		o.gVector.setMap(null);
-	});
-	
-	odd.layers[layerName].features.length = 0;
-	
-	$("#count-" + layerName).html(0).hide();
-	
-}*/
 
 /**
  * Calculates the distance between two latlng points in km.
